a28ef26b899e8ff2edc3789accc7f2bc56f0a11f
Adil Zafar <adil.khan@primeeng.ca>
2023-05-18 12:15:01 -0700

    Add new constants

--- src/consts.py
+++ src/consts.py
@@ -1,7 +1,17 @@
-# These are absolute URLs
 NETSUITE_URL = r"https://6516658-sb1.app.netsuite.com/app/common/custom/custrecordentry.nl?rectype=207"
-SHAREPOINT_URL = r"https://primevic.sharepoint.com/sites/InternalDev"
+CHROME_USER_PROFILE = r'C:\Users\adil.khan\AppData\Local\Google\Chrome\User Data - Copy'
+TAKEOFF_PATH = r"D:\Quotes\XXXX_takeoff_1.11.5.xltm"
+CHECKLIST_PATH = r"D:\Quotes\Template_Job Opening Checklist_latest.xltm"
+CONFIG_PATH = r'D:\Quotes\CONFIGURATOR_latest.xltm'
+DROPDOWN_DIR = r'A:/Templates/Take Off Templates/NetSuite data'
+JOB_DIRS = ['Correspondence', 'Info to B drive', 'Purchase Order', 'RFQ', 'Specifications', 'Submittal']
 
-# These are URLs relative to the site
-TAKEOFF_URL = r"Shared Documents/Quotes/XXXX-takeoff_1.11.5.xltm"
-CHECKLIST_URL = r"Shared Documents/Quotes/Template_Job Opening Checklist_latest.xltm"
+DROPDOWN_PATHS = {
+    'customers': f'{DROPDOWN_DIR}/NetSuite_Daily_Customer_List.csv',
+    'departments': f'{DROPDOWN_DIR}/NetSuite_Daily_Department_List.csv',
+    'classes': f'{DROPDOWN_DIR}/NetSuite_Daily_Class_List.csv',
+    'reps': f'{DROPDOWN_DIR}/NetSuite_Daily_SalesRep_List.csv',
+    'items': f'{DROPDOWN_DIR}/NetSuite_Daily_Item_List.csv',
+    'templates': f'{DROPDOWN_DIR}/NetSuite_Daily_Project_Template_List.csv',
+    'types': './data/dropdowns/ProjectTypes.csv',
+}
