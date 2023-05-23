361493f10ee1012483c85a5a9837d734822c1fe8
Adil Zafar <adil.khan@primeeng.ca>
2023-05-23 15:23:53 -0700

    Change consts

--- src/consts.py
+++ src/consts.py
@@ -1,8 +1,8 @@
 NETSUITE_URL = r"https://6516658-sb1.app.netsuite.com/app/common/custom/custrecordentry.nl?rectype=207"
 CHROME_USER_PROFILE = r'C:\Users\adil.khan\AppData\Local\Google\Chrome\User Data - Copy'
-TAKEOFF_PATH = r"D:\Quotes\XXXX_takeoff_1.11.5.xltm"
-CHECKLIST_PATH = r"D:\Quotes\Template_Job Opening Checklist_latest.xltm"
-CONFIG_PATH = r'D:\Quotes\CONFIGURATOR_latest.xltm'
+TAKEOFF_PATH = r"A:\Templates\Take Off Templates\XXXX-takeoff_1.11.5.xltm"
+CHECKLIST_PATH = r"Q:\Templates\Job Opening Checklist\Template_Job Opening Checklist_latest.xltm"
+CONFIG_PATH = r'B:\Prime Products\CATALOG\CONFIGURATOR_latest.xltm'
 DROPDOWN_DIR = r'A:/Templates/Take Off Templates/NetSuite data'
 JOB_DIRS = ['Correspondence', 'Info to B drive', 'Purchase Order', 'RFQ', 'Specifications', 'Submittal']
 
@@ -13,5 +13,5 @@ DROPDOWN_PATHS = {
     'reps': f'{DROPDOWN_DIR}/NetSuite_Daily_SalesRep_List.csv',
     'items': f'{DROPDOWN_DIR}/NetSuite_Daily_Item_List.csv',
     'templates': f'{DROPDOWN_DIR}/NetSuite_Daily_Project_Template_List.csv',
-    'types': './data/dropdowns/ProjectTypes.csv',
+    'types': f'{DROPDOWN_DIR}/NetSuite_Project_Type_List.csv',
 }
