dd021ff5121f45cacf87e623fd911fcb54173f52
Adil Zafar <adil.khan@primeeng.ca>
2023-05-25 16:52:41 -0700

    Add docstring

--- src/utility/list_handler.py
+++ src/utility/list_handler.py
@@ -1,9 +1,10 @@
 def add_keys_to_elements(keys, elements, start=0):
     """
+    Append the user keys to the elements.
 
-    :param keys:
-    :param elements:
-    :param start:
+    :param keys: keys entered by the user in the app
+    :param elements: web elements required by the controller
+    :param start: element index to start appending keys from
     """
     for i in range(start, len(elements)):
         if 'action' in elements[i]:
