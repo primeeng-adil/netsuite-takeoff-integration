e82ce092c4b7b1cd853f6957b8b63bfaec35b9b7
Adil Zafar <adil.khan@primeeng.ca>
2023-05-29 15:29:32 -0700

    Modify middleware.py to reflect specifications

--- src/middleware/middleware.py
+++ src/middleware/middleware.py
@@ -1,113 +1,92 @@
-import re
-from pathlib import Path
-from selenium.webdriver import Keys
-from src.sharepoint.dir_handler import DirHandler
-from src.utility import list_handler
+import chromedriver_autoinstaller
+import src.middleware.utils as utils
 from pywebgo.controller import WebController
-from .elements import get_elements, set_user_pass_questions
-from src.consts import *
-
-
-def modify_data_keys(data_keys: list) -> None:
-    """
-    Modify keys list to meet custom requirements.
-
-    :param data_keys: keys sent in by the user through the app interface
-    """
-    data_keys[7] += '_' + data_keys.pop(8) + '_' + data_keys.pop(8)
-    data_keys.insert(7, data_keys[6].replace('SALES - ', ''))
-    for i in range(len(data_keys)):
-        data_keys[i] += Keys.TAB
-    data_keys.insert(7, Keys.TAB)
-    data_keys.insert(11, 'DONE')
-    data_keys[-1] += Keys.TAB
-
-
-def get_proj_id(controller: WebController) -> str:
-    """
-    Find and return the job id of the current project.
-
-    :param controller: WebController instance containing the current elements list
-    :return: job id of the current project
-    """
-    proj_id = None
-    if controller.data_handler.retrieve_data():
-        proj_title = controller.data_handler.retrieve_data()[0]['data-keys']
-        proj_id = re.search(r'^\S\d+', proj_title)
-    if proj_id:
-        return proj_id.group()
-
-
-def generate_element_list(data_keys: list) -> tuple:
-    """
-    Append keys to their respective elements and return elements and sharepoint path.
-
-    :param data_keys: keys sent in by the user through the app interface
-    :return: elements for WebController and relative sharepoint path for the project
-    """
-    set_user_pass_questions(data_keys)
-    sharepoint_path = data_keys.pop(14)
-    modify_data_keys(data_keys)
-    elements = get_elements()
-    list_handler.add_keys_to_elements(data_keys, elements, start=4)
-    return elements, sharepoint_path
-
-
-def make_sharepoint_dirs(sharepoint_path: str, proj_id: str, proj_name: str, proj_item: str, creds: tuple) -> None:
-    """
-    Create project directories and files in SharePoint.
-
-    :param sharepoint_path: relative SharePoint path to create site directory in
-    :param proj_id: job id of the current project
-    :param proj_name: job name of the current project
-    :param proj_item: item type of the current project
-    :param creds: username and password of the current user
-    """
-    root_url = "/sites/InternalDev/"
-    site_url = f"{sharepoint_path}/{proj_name}"
-    proj_url = f"{site_url}/{proj_id}_{proj_item}"
-    takeoff_new_name = Path(TAKEOFF_URL).parts[-1].replace('XXXX-', f'{proj_id}_{proj_item}_')
-    checklist_new_name = f'{proj_id}_Job Opening Checklist_1.0.0.xltm'
-    dirs = ['Correspondence', 'Info to B drive', 'Purchase Order', 'RFQ', 'Specifications', 'Submittal']
-
-    dir_handler = DirHandler(SHAREPOINT_URL, creds)
-    proj_url = Path(dir_handler.make_proj_dir(proj_url))
-    proj_rel_url = str(proj_url.relative_to(Path(root_url)))
-    dir_handler.make_proj_subdir(dirs, proj_rel_url)
-    takeoff_dest_url = str(Path(proj_rel_url, takeoff_new_name))
-    checklist_dest_url = str(Path(proj_rel_url, 'Info to B drive', checklist_new_name))
-    dir_handler.copy_file(TAKEOFF_URL, takeoff_dest_url)
-    dir_handler.copy_file(CHECKLIST_URL, checklist_dest_url)
+from src.consts import CHROME_USER_PROFILE, NETSUITE_URL
 
 
 def execute_controller(url: list, elements: list) -> WebController:
     """
-    Execute WebController processes.
+    Execute controller processes.
 
     :param url: URL of the landing page
     :param elements: elements for the WebController to process
     :return: instance of WebController
     """
-    web_controller = WebController(url, wait=0.3)
+    options = [
+        f'user-data-dir={CHROME_USER_PROFILE}',
+        'start-maximized'
+    ]
+    web_controller = WebController(url, options=options, wait=0.2)
     web_controller.run_controller(elements)
     return web_controller
 
 
-def execute_sharepoint(sharepoint_path: str, controller: WebController, elements: list) -> None:
+def get_proj_data(controller, elements, proj_options):
     """
-    Execute SharePoint processes.
 
-    :param sharepoint_path: relative sharepoint path to save the current project to
-    :param controller: WebController instance containing the current elements list
-    :param elements: elements passed into the WebController classed
+    :param controller: current instance of WebController
+    :param elements: elements for the project
+    :param proj_options: user specified options for the project
+    :return: data for the project
     """
-    proj_id = get_proj_id(controller)
-    username = elements[0]['keys']
-    password = elements[1]['keys'][:-1]
+    proj_id = utils.get_proj_id(controller)
+    proj_client = elements[4]['keys'][:-1]
+    proj_rep = elements[7]['keys'][:-1]
     proj_item = elements[15]['keys'][:-1]
     proj_full_name = elements[18]['keys'][:-1]
+    proj_type = elements[24]['keys'][:-1]
     proj_name = proj_full_name[proj_full_name.find('_') + 1:]
-    make_sharepoint_dirs(sharepoint_path, proj_id, proj_name, proj_item, (username, password))
+    proj_url = controller.data_handler.database[1]['data-keys']
+
+    proj_data = {
+        'id': proj_id,
+        'name': proj_name,
+        'item': proj_item,
+        'type': proj_type,
+        'rep': proj_rep,
+        'client': proj_client,
+        'url': proj_url
+    }
+
+    proj_data.update(proj_options)
+    return proj_data
+
+
+def get_proj_options(data_keys: list) -> dict:
+    """
+    Get the user specified options for the project.
+
+    :param data_keys: keys sent in by the user through the app interface
+    :return: user specified options
+    """
+    is_logged = data_keys.pop()
+    has_config = data_keys.pop()
+    proj_path = data_keys.pop()
+
+    return {
+        'path': proj_path,
+        'config': has_config,
+        'log': is_logged
+    }
+
+
+def execute_dirs_files_maker(proj_data: dict) -> None:
+    """
+    Create project files and directories.
+
+    :param proj_data: data for the project
+    """
+    utils.make_project_dirs_files(proj_data)
+
+
+def update_quote_log(proj_data):
+    """
+    Update the Quote Log if the user specified.
+
+    :param proj_data: data for the project
+    """
+    if proj_data['log']:
+        utils.update_quote_log(proj_data)
 
 
 def run_middleware(app) -> None:
@@ -117,14 +96,22 @@ def run_middleware(app) -> None:
     :param app: current app object interacting with the user
     """
     app.start_progress()
-    data = app.get_data()
-    app.update_progress('Creating elements', 10)
-    elements, sharepoint_path = generate_element_list(data)
+    data_keys = app.get_data_keys()
+    proj_options = get_proj_options(data_keys)
+
+    app.update_progress('Installing Chrome Driver', 5)
+    chromedriver_autoinstaller.install()
+    app.update_progress('Creating controller elements', 5)
+    elements = utils.generate_element_list(data_keys.copy())
     app.update_progress('Executing controller', 10)
     controller = execute_controller([NETSUITE_URL], elements)
-    app.update_progress('Creating SharePoint directories', 60)
-    execute_sharepoint(sharepoint_path, controller, elements)
+    proj_data = get_proj_data(controller, elements, proj_options)
+    app.update_progress('Creating project files and directories', 40)
+    execute_dirs_files_maker(proj_data)
+    app.update_progress('Updating the quote log', 20)
+    update_quote_log(proj_data)
     app.update_progress('Finishing', 20)
+
     controller.close()
     app.stop_progress()
     app.show_success_msg()
