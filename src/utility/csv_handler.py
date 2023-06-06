280b0acf1e9223547453df369118dd4b8aaa9038
Adil Zafar <adil.khan@primeeng.ca>
2023-06-06 12:42:45 -0700

    Add option to include/exclude header row

--- src/utility/csv_handler.py
+++ src/utility/csv_handler.py
@@ -2,11 +2,12 @@ import csv
 from pathlib import Path
 
 
-def read_csv(path: Path | str) -> list:
+def read_csv(path: Path | str, header: bool = False) -> list:
     """
     Reads and returns data from a csv file.
 
     :param path: path of the csv to read from
+    :param header: flag to check if header row exists
     :return: a two-dimensional list containing rows and columns
     """
     with open(path, newline='') as csvfile:
@@ -14,17 +15,21 @@ def read_csv(path: Path | str) -> list:
         reader = csv.reader(csvfile)
         for row in reader:
             data_list.append(row)
+        if header:
+            data_list.pop(0)
         return data_list
 
 
-def read_csv_column(path: Path | str, column: int = 0) -> list:
+def read_csv_column(path: Path | str, column: int = 0, header: bool = False) -> list:
     """
     Reads and returns data from a csv file.
 
     :param path: path of the csv to read from
+    :param column: index of the column to be retrieved
+    :param header: flag to check if header row exists
     :return: a two-dimensional list containing rows and columns
     """
-    return [row[column] for row in read_csv(path)]
+    return [row[column] for row in read_csv(path, header)]
 
 
 def write_csv(path: Path | str, data: list) -> None:
