be335881d689bf6bc6496c0d767c3aaff512ae9a
Adil Zafar <adil.khan@primeeng.ca>
2023-06-30 16:06:43 -0700

    Read global constants from a csv that is accessible to the user

--- src/consts.py
+++ src/consts.py
@@ -1,21 +1,50 @@
-NETSUITE_URL = r"https://6516658-sb1.app.netsuite.com/app/common/custom/custrecordentry.nl?rectype=207"
-CHROME_USER_PROFILE = r'C:\Users\adil.khan\AppData\Local\Google\Chrome\User Data - Copy'
-TAKEOFF_PATH = r"A:\Templates\Take Off Templates\XXXX-takeoff_1.11.5.xltm"
-CHECKLIST_PATH = r"Q:\Templates\Job Opening Checklist\Template_Job Opening Checklist_latest.xltm"
-CONFIG_PATH = r'B:\Prime Products\CATALOG\CONFIGURATOR_latest.xltm'
-QUOTE_LOG_PATH = r'D:\Quotes\Quote Log rev1.16.xlsm'
-DROPDOWN_DIR = r'A:/Templates/Take Off Templates/NetSuite data'
-JOB_DIRS = ['Correspondence', 'Info to B drive', 'Purchase Order', 'RFQ', 'Specifications', 'Submittal']
-GITHUB_SRC = r"https://github.com/primeeng-adil/netsuite-takeoff-integration"
-GITHUB_RLS = r"https://github.com/primeeng-adil/netsuite-takeoff-integration/releases"
+import csv
 
-DROPDOWN_PATHS = {
-    'addresses': f'{DROPDOWN_DIR}/NetSuite_Daily_SiteAddress_List.csv',
-    'customers': f'{DROPDOWN_DIR}/NetSuite_Daily_Customer_List.csv',
-    'departments': f'{DROPDOWN_DIR}/NetSuite_Daily_Department_List.csv',
-    'classes': f'{DROPDOWN_DIR}/NetSuite_Daily_Class_List.csv',
-    'reps': f'{DROPDOWN_DIR}/NetSuite_Daily_SalesRep_List.csv',
-    'items': f'{DROPDOWN_DIR}/NetSuite_Daily_Item_List.csv',
-    'templates': f'{DROPDOWN_DIR}/NetSuite_Daily_Project_Template_List.csv',
-    'types': f'{DROPDOWN_DIR}/NetSuite_Project_Type_List.csv',
-}
+NETSUITE_URL = None
+GITHUB_SRC = None
+CHROME_USER_PROFILE = None
+TAKEOFF_PATH = None
+CHECKLIST_PATH = None
+CONFIG_PATH = None
+QUOTE_LOG_PATH = None
+DROPDOWN_DIR = None
+JOB_DIRS = None
+DROPDOWN_PATHS = None
+
+
+def get_consts_from_csv():
+    reader = csv.DictReader(open('./data/consts.csv', encoding='utf-8-sig'))
+    result = {}
+    for row in reader:
+        result[row['VAR']] = row['VAL']
+
+    global NETSUITE_URL
+    global GITHUB_SRC
+    global CHROME_USER_PROFILE
+    global TAKEOFF_PATH
+    global CHECKLIST_PATH
+    global CONFIG_PATH
+    global QUOTE_LOG_PATH
+    global DROPDOWN_DIR
+    global JOB_DIRS
+    global DROPDOWN_PATHS
+
+    NETSUITE_URL = result['NETSUITE_URL']
+    GITHUB_SRC = result['GITHUB_SRC']
+    CHROME_USER_PROFILE = result['CHROME_USER_PROFILE']
+    TAKEOFF_PATH = result['TAKEOFF_PATH']
+    CHECKLIST_PATH = result['CHECKLIST_PATH']
+    CONFIG_PATH = result['CONFIG_PATH']
+    QUOTE_LOG_PATH = result['QUOTE_LOG_PATH']
+    DROPDOWN_DIR = result['DROPDOWN_DIR']
+    JOB_DIRS = [x.strip() for x in result['JOB_DIRS'].split(',')]
+    DROPDOWN_PATHS = {
+        'addresses': f'{DROPDOWN_DIR}/NetSuite_Daily_SiteAddress_List.csv',
+        'customers': f'{DROPDOWN_DIR}/NetSuite_Daily_Customer_List.csv',
+        'departments': f'{DROPDOWN_DIR}/NetSuite_Daily_Department_List.csv',
+        'classes': f'{DROPDOWN_DIR}/NetSuite_Daily_Class_List.csv',
+        'reps': f'{DROPDOWN_DIR}/NetSuite_Daily_SalesRep_List.csv',
+        'items': f'{DROPDOWN_DIR}/NetSuite_Daily_Item_List.csv',
+        'templates': f'{DROPDOWN_DIR}/NetSuite_Daily_Project_Template_List.csv',
+        'types': f'{DROPDOWN_DIR}/NetSuite_Project_Type_List.csv',
+    }
